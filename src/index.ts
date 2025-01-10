2fac9c40357b72afec105352160e8c778c5357e8
Faizan Raza <faizanms025347@gmail.com>
2025-01-11 00:20:22 +0530

    added cors

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,20 @@ import connectDB from './db/db'
 import authRoutes from './routes/auth.routes'
 import contentRoutes from './routes/content.routes'
 import shareRoutes from './routes/share.routes'
+const cors = require('cors')
 
 const app = express()
 const PORT = process.env.PORT || 4000
 app.use(express.json())
 
+const corsOptions = {
+    origin: 'https://hivee-mind.vercel.app',
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],  
+    credentials: true  
+
+};
+app.use(cors('*',corsOptions));
 
 connectDB()
 app.use('/', authRoutes)
