62da29a1c58fd06b9942ac461b1cb5b0b5c5c611
Faizan <faizanms025347@gmail.com>
2025-02-01 16:57:20 +0530

    updates

--- src/controllers/auth.controllers.ts
+++ src/controllers/auth.controllers.ts
@@ -57,9 +57,9 @@ exports.signup =  async (req: Request, res: Response) => {
           console.log("reached here 2")
 
           res.status(200).json({message:`${user.name} has been successfully registered.`,id:user._id})
-    } catch (error) {
-        console.log(error)
-    }
+    } catch (error:any) {
+      console.log({message:`${error.message}`})
+  }
 }
 
 exports.login = async (req: Request, res: Response) => {
